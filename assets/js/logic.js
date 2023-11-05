c9a1b4150aa18874ae817c03ec574013591953d0
cariberrii <ca.fillingham@gmail.com>
2023-11-05 16:05:53 +0000

    Adds timer function and displays countdown on page

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -2,38 +2,66 @@
 
 var startButton = document.querySelector("#start");
 var startScreenEl = document.querySelector("#start-screen");
-var timerEl = document.querySelector("#timer");
+var timerEl = document.querySelector("#time");
 var questionsEl = document.querySelector("#questions");
 var choicesEl = document.querySelector("#choices");
+
+// Timer variables
+
+var timerCount;
+var timer;
+console.log(timerCount);
 
 // Start Quiz
 
 function startQuiz() {
+    timerCount = quizQuestions.length * 15;
     startScreenEl.setAttribute("class", "hide");
     questionsEl.removeAttribute("class");
     getQuestions();
+    startTimer();
 }
 startButton.addEventListener("click", startQuiz);
+
+// Start Timer
+function startTimer() {
+  timer = setInterval(function () {
+    timerCount--;
+    timerEl.textContent = timerCount;
+  }, 1000);
+  console.log(timerCount);
+}
 
 // Quiz variables
 var questionIndex = 0;
 
 // Loop through questions and answer choices as list elements
 function getQuestions() {
-    // iterate over the list of questions
-    var activeQuestion = quizQuestions[questionIndex];
-    var questionTitle = document.getElementById("question-title");
-    questionTitle.textContent = activeQuestion.question;
-    choicesEl.innerHTML = "";
-    activeQuestion.answers.forEach(function (answer, i) {
-        var answerButton = document.createElement("button");
-        answerButton.setAttribute("value", answer);
-        answerButton.textContent = answer;
-        // answerButton.addEventListener("click", userAnswer); // userAnswer is next function
-        choicesEl.appendChild(answerButton);
-    });
+  // iterate over the list of questions
+  var activeQuestion = quizQuestions[questionIndex];
+  var questionTitle = document.getElementById("question-title");
+  questionTitle.textContent = activeQuestion.question;
+  choicesEl.innerHTML = "";
+  activeQuestion.answers.forEach(function (answer, i) {
+    var answerButton = document.createElement("button");
+    answerButton.setAttribute("value", answer);
+    answerButton.textContent = answer;
+    answerButton.addEventListener("click", userAnswer);
+    choicesEl.appendChild(answerButton);
+  });
 }
-// if a user clicked on an answer
+// User Answer Check to run in getQuestions function
+function userAnswer() {
+  // answerButton.preventDefault();
+  if (this.value !== quizQuestions[questionIndex].correctAnswerIndex) {
+    // deduct time
+    // end quiz if time is 0 or less
+    // update timer on page
+    // tell user Wrong! in red text
+  } else {
+    // tell user Correct! in green text
+  }
+}
 // check the index of the answer
 // compare that value to the correct answer
 // if (the thing the user answered is correct)
