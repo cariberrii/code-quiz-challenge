ee94eb6b626c1dc5de6a35a67dcc1dcf36c548a5
cariberrii <ca.fillingham@gmail.com>
2023-11-05 12:23:08 +0000

    Adds JS to start quiz, hides start screen and shows first question

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,6 +1,33 @@
+// get DOM elements
+
+var startButton = document.querySelector("#start");
+var startScreenEl = document.querySelector("#start-screen");
+var timerEl = document.querySelector("#timer");
+var questionsEl = document.querySelector("#questions");
+var choicesEl = document.querySelector("#choices");
+
+// Start Quiz
+
+function startQuiz() {
+    startScreenEl.setAttribute("class", "hide");
+    questionsEl.removeAttribute("class");
+    getQuestions();
+}
+startButton.addEventListener("click", startQuiz);
+
+
+// Quiz variables
+var questionIndex = 0;
+
+// Loop through questions and answer choices as list elements
+function getQuestions() {
 // iterate over the list of questions
+var activeQuestion = quizQuestions[questionIndex];
+questionsEl.textContent = activeQuestion.question;
+quizQuestions.forEach((element) => console.log(element));
+}
     // if a user clicked on an answer
-    // check the text of the answer (or maybe some data property)
+    // check the index of the answer
     // compare that value to the correct answer
     // if (the thing the user answered is correct)
     //    display correct!
