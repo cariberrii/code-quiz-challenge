67a142a08a8fd02c7ca729052030247da910f998
cariberrii <ca.fillingham@gmail.com>
2023-11-05 09:15:29 +0000

    Adds HTML, assets and quiz questions content in JS

---
+++ assets/js/questions.js
@@ -0,0 +1,66 @@
+var quizQuestions = [
+    {
+        question: "Commonly used datatypes DO NOT include:",
+        answers: ["strings",
+            "booleans",
+            "alerts",
+            "numbers"],
+            correctAnswerIndex: 2
+    },
+    {
+        question: "The condition in an if/else statement is enclosed within ____.",
+        answers: ["quotes",
+            "curly brackets",
+            "parentheses",
+            "square brackets"],
+            correctAnswerIndex: 2
+    },
+    {
+        question: "Arrays in JavaScript can be used to store ____.",
+        answers: ["numbers and strings",
+            "other arrays",
+            "booleans",
+            "all of the above"],
+            correctAnswerIndex: 3
+    },
+    {
+        question: "String values must be enclosed within ____ when being assigned to variables.",
+        answers: ["commas",
+            "curly brackets",
+            "quotes",
+            "parentheses"],
+            correctAnswerIndex: 2
+    },
+    {
+        question: "A very useful tool used during development and debugging for printing content to the debugger is:",
+        answers: ["JavaScript",
+            "terminal/bash",
+            "for loops",
+            "console.log"],
+            correctAnswerIndex: 3
+    },
+    {
+        question: "Inside which HTML element do we put the JavaScript?",
+        answers: ["<javascript>",
+            "<js>",
+            "<script>",
+            "<scripting>"],
+            correctAnswerIndex: 2
+    },
+    {
+        question: "How do you call a function named myFunction?",
+        answers: ["call myFunction()",
+            "muFunction()",
+            "call function myFunction",
+            "Call.myFunction"],
+            correctAnswerIndex: 2
+    },
+    {
+        question: "How does a for loop start?",
+        answers: ["for (i = 0; i <= 5; i++)",
+            "for (i = 0; i <= 5)",
+            "for i = 1 to 5",
+            "for (i <= 5; i++)"],
+            correctAnswerIndex: 0
+    },
+]
---
+++ assets/js/scores.js
@@ -0,0 +1,5 @@
+console.log("this is scores.js");
+// console.log(quizQuestions);
+
+var myTestItem = localStorage.getItem("testItem");
+alert(myTestItem);
